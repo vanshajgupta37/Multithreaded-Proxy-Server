1a75e1f85020fd01ed14e4b786f42517ef8f1b23
Vanshaj <vanshajgupta37@gmail.com>
2024-09-04 19:05:43 +0530

    Update proxy_server.c

--- proxy_server.c
+++ proxy_server.c
@@ -21,7 +21,9 @@
 pthread_t t_id[MAX_CLIENTS];
 int port_num = 8080;
 int proxy_socket_ID;
-
+sem_t semaphore;
+pthread_mutex_t lock;
+ 
 typedef struct cache_store cache_store;
 
 struct cache_store{
@@ -36,3 +38,23 @@ struct cache_store{
 cache_store* find(char* url);
 int add_cache_store(char* data, int size, char* url);
 void remove_cache_store();
+
+cache_store* head;
+int cache_size;
+
+int main(int argc, char* argv[]){
+    int client_socketID, client_len;
+    struct sochaddr server_addr, client_addr;
+    sem_init(&semaphore,0,  MAX_CLIENTS);
+
+    pthread_mutex_init(&lock, NULL);
+    if(argv == 2){
+        port_number = atoi(argv[1]);
+    }
+    else{
+        printf("Arguments provided are less than required\nTry Again with proper arguments!!\n");
+        exit(1);
+    }
+
+    
+}
