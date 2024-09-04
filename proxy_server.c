221a50f16f70052b5d814daea7c5d730ea4994d7
Vanshaj <vanshajgupta37@gmail.com>
2024-09-04 14:13:16 +0530

    Update c_cpp_properties.json and including necessary libraries in proxy_server.c

--- proxy_server.c
+++ proxy_server.c
@@ -1,8 +1,20 @@
 #include "http_parser.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <semaphore.h>
 #include <pthread.h>
+
 
 #define MAX_CLIENTS 20
 
